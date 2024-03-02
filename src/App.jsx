8f9d98a2a24ad1f32959656b316dcdf534096983
Sina Panahimoradkandi <s.panahi.m@gmail.com>
2024-03-01 19:49:52 -0500

    minor changes and tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "./App.css";
 import CubeBackground from "./components/CubeBackground.jsx";
+import TwoColumnLayout from "./components/TwoColumnLayout.jsx";
+import OneColumnLayout from "./components/OneColumnLayout.jsx";
+import Header from "./components/Header.jsx";
+import Navigation from "./components/Navigation.jsx";
 
 function App() {
-    return <CubeBackground></CubeBackground>;
+    return (
+        <CubeBackground>
+            <TwoColumnLayout>
+                <OneColumnLayout>
+                    <Header />
+                    <Navigation />
+                </OneColumnLayout>
+                <OneColumnLayout></OneColumnLayout>
+            </TwoColumnLayout>
+        </CubeBackground>
+    );
 }
 
 export default App;
--- src/components/CubeBackground.jsx
+++ src/components/CubeBackground.jsx
@@ -3,6 +3,7 @@ import styles from "./cubeBackground.module.css";
 export default function CubeBackground({ children }) {
     return (
         <div className={styles.area}>
+            {children}
             <div>
                 <ul className={styles.circles}>
                     <li></li>
@@ -17,7 +18,6 @@ export default function CubeBackground({ children }) {
                     <li></li>
                 </ul>
             </div>
-            {children}
         </div>
     );
 }
