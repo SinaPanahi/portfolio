34c6fbc1c7b688da39f2b35398a2f2b484959301
Sina Panahimoradkandi <s.panahi.m@gmail.com>
2024-03-01 19:45:41 -0500

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from "./header.module.css";
+export default function Header() {
+    return (
+        // <header className={styles.header}>
+        <header className={styles.header}>
+            <h1>Sina Panahi</h1>
+            <h4>Web Developer</h4>
+            <p>
+                I build pixel-perfect, engaging, and accessible digital
+                experiences.
+            </p>
+        </header>
+    );
+}
