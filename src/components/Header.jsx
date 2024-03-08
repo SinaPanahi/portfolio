85b3e2c45d2b0fa09fc8bfbf65c7a224fdc5341a
Sina Panahimoradkandi <s.panahi.m@gmail.com>
2024-03-08 12:38:24 -0500

    added a link for downloading resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import EmailIcon from "./icons/EmailIcon";
-import GithubIcon from "./icons/GithubIcon";
-import LinkedinIcon from "./icons/LinkedinIcon";
+import { FaGithub } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFilePdf } from "react-icons/fa6";
+import resume from "./Sina_Panahimoradkandi_Resume.pdf";
 
 export default function Header() {
     return (
-        // <header className={styles.header}>
         <header className="p-5 leading-5 lg:p-10 lg:leading-10 h-min text-slate-300">
             <h1 className="text-5xl lg:text-6xl">Sina Panahi</h1>
             <h4 className="text-l lg:text-xl font-bold">Web Developer</h4>
@@ -13,17 +14,21 @@ export default function Header() {
             </p>
             <div className="flex mt-5 justify-between pr-10">
                 <a href="mailto:s.panahi.m@gmail.com">
-                    <EmailIcon className="w-8 h-9 stroke-slate-300 stroke-1 fill-transparent" />
+                    <HiMail className="w-9 h-9 text-slate-300" />
                 </a>
 
                 <a href="https://github.com/SinaPanahi" target="_blank">
-                    <GithubIcon className="w-9 h-9 stroke-slate-300 stroke-1 fill-transparent" />
+                    <FaGithub className="w-9 h-9 text-slate-300" />
                 </a>
                 <a
                     href="https://www.linkedin.com/in/sina-panahimoradkandi-630a3283/"
                     target="_blank"
                 >
-                    <LinkedinIcon className="w-8 h-8 stroke-slate-300 stroke-1 fill-transparent" />
+                    <FaLinkedin className="w-9 h-9 text-slate-300" />
+                </a>
+
+                <a href={resume} download>
+                    <FaFilePdf className="w-9 h-9 text-slate-300" />
                 </a>
             </div>
         </header>
