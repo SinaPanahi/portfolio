f2da72e533d41397ba96361c3ad7506e132cb53a
Sina Panahimoradkandi <s.panahi.m@gmail.com>
2024-03-09 09:37:23 -0500

    added a decorative border component, completed skill icons

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import TurningBorder from "./components/TurningBorder.jsx";
 import LavaBackground from "./components/LavaBackground.jsx";
 import ExperienceWrapper from "./components/ExperienceWrapper.jsx";
 import SkillsWrapper from "./components/SkillsWrapper.jsx";
+import DecorativeBorder from "./components/DecorativeBorder.jsx";
+
+import { Tooltip } from "react-tooltip";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
@@ -26,6 +29,11 @@ import { SiFlask } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
 import { SiMariadb } from "react-icons/si";
+import { SiJquery } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { SiAdobephotoshop } from "react-icons/si";
+import { SiAdobeillustrator } from "react-icons/si";
+import { CgFigma } from "react-icons/cg";
 
 function App() {
     return (
@@ -40,27 +48,33 @@ function App() {
                 <About />
                 <ExperienceWrapper />
                 <SkillsWrapper>
-                    <FaHtml5 className="w-20 h-20" />
-                    <FaCss3Alt className="w-20 h-20" />
-                    <SiJavascript className="w-20 h-20 rounded-md" />
-                    <SiTypescript className="w-20 h-20" />
-                    <FaReact className="w-20 h-20" />
-                    <SiNextdotjs className="w-20 h-20" />
-                    <FaBootstrap className="w-24 h-24" />
-                    <SiTailwindcss className="w-20 h-20" />
-                    <FaPython className="w-20 h-20" />
-                    <SiFlask className="w-20 h-20" />
-                    <SiCsharp className="w-20 h-20" />
-                    <SiDotnet className="w-20 h-20" />
-                    <SiPhp className="w-20 h-20" />
-                    <PiFileSqlFill className="w-20 h-20" />
-                    <SiMongodb className="w-20 h-20" />
-                    <BiLogoPostgresql className="w-20 h-20" />
-                    <SiSqlite className="w-20 h-20" />
-                    <SiMariadb className="w-20 h-20" />
+                    <FaHtml5 className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <FaCss3Alt className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiJavascript className="w-8 h-8 lg:w-12 lg:h-12 rounded-md" />
+                    <SiTypescript className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <FaReact className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <FaGithub className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiNextdotjs className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <FaBootstrap className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiTailwindcss className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <FaPython className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiFlask className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiCsharp className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiDotnet className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiPhp className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <PiFileSqlFill className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiMongodb className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <BiLogoPostgresql className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiSqlite className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiMariadb className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiJquery className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiAdobephotoshop className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <SiAdobeillustrator className="w-8 h-8 lg:w-12 lg:h-12" />
+                    <CgFigma className="w-8 h-8 lg:w-12 lg:h-12" />
                 </SkillsWrapper>
             </RightPanel>
             <LavaBackground />
+            <DecorativeBorder />
         </>
     );
 }
